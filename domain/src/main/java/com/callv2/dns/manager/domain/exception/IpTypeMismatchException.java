adffdd7d40edafc72f29797678e95a9e0a489623
jhonatapers <jhonatapers@gmail.com>
2025-04-26 10:58:33 -0300

    fix: remove String.format
    https://github.com/CallVDois/dns-manager/pull/7#discussion_r2061318075

--- domain/src/main/java/com/callv2/dns/manager/domain/exception/IpTypeMismatchException.java
+++ domain/src/main/java/com/callv2/dns/manager/domain/exception/IpTypeMismatchException.java
@@ -9,8 +9,7 @@ public class IpTypeMismatchException extends DomainException {
     private static final String MESSAGE = "IP type mismatch.";
 
     private IpTypeMismatchException(final IpType expected, final IpType actual) {
-        super(String.format(MESSAGE, expected, actual),
-                List.of(DomainException.Error.with("Expected %s but got %s".formatted(expected, actual))));
+        super(MESSAGE, List.of(DomainException.Error.with("Expected %s but got %s".formatted(expected, actual))));
     }
 
     public static IpTypeMismatchException with(final IpType expected, final IpType actual) {
